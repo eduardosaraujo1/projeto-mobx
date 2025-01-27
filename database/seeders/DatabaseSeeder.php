71b734a05cbeb0bba01c50b1dfa10064d076a601
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-01-27 19:40:07 -0300

    refactor(database): extract complex closure to its own function

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,10 +9,36 @@ use App\Models\Imovel;
 use App\Models\ImovelDocument;
 use App\Models\ImovelLog;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * Creates the Collection to be attached to the BelongsToMany relationship between User and Imobiliaria
+     * First it chooses some imobiliarias from the parameter `imobiliarias`
+     * Then it lists all the IDs of the imobiliarias
+     * Then it turns the id array into an array of associative arrays with the `level` property for the pivot table
+     * @param \Illuminate\Database\Eloquent\Collection<int, Imobiliaria> $imobiliarias
+     * @return \Illuminate\Support\Collection<int|string, array{level: int|string>}
+     */
+    private static function generateUserImobiliarias(Collection $imobiliarias)
+    {
+        // Get random imobiliaria list with random length
+        $userImobiliarias = $imobiliarias->random(rand(min: 1, max: 3));
+
+        // BelongsToMany->Attach only needs the IDs, so remove the unnecessary info with pluck
+        $ids = $userImobiliarias->pluck('id');
+
+        // BelongsToMany->Attach accepts associative array with the other properties of the pivot table
+        // read https://laravel.com/docs/11.x/eloquent-relationships#updating-many-to-many-relationships
+        $attachments = $ids->mapWithKeys(fn($id) => [
+            $id => ['level' => AccessLevel::randomId()]
+        ]);
+
+        return $attachments;
+    }
+
     /**
      * Seed the application's database.
      */
@@ -27,9 +53,6 @@ class DatabaseSeeder extends Seeder
         ]);
 
         // Regular users
-        /**
-         * @var \Illuminate\Database\Eloquent\Collection<int, User>
-         */
         $users = User::factory()->count(15)->create();
 
         // Imobiliarias
@@ -39,21 +62,10 @@ class DatabaseSeeder extends Seeder
         $users
             ->shift() // remove first user (admin)
             ->each(
-                function (User $user) use ($imobiliarias): void {
-                    // Get random imobiliaria(s), count between 1 and 3, for each user
-                    $userImobiliarias = $imobiliarias->random(rand(min: 1, max: 3));
-
-                    // attach only needs the IDs, so remove the unnecessary info with pluck
-                    $ids = $userImobiliarias->pluck('id');
-
-                    // To pass the 'level' parameter, each value from array must be associated with the array of column values
-                    // read https://laravel.com/docs/11.x/eloquent-relationships#updating-many-to-many-relationships (For convenience, attach and detach also accept arrays of IDs as input)
-                    $attach = $ids->mapWithKeys(fn($id) => [
-                        $id => ['level' => AccessLevel::randomId()]
-                    ]);
-
+                function (User $user) use ($imobiliarias) {
+                    $attachments = static::generateUserImobiliarias($imobiliarias);
                     // attach the imobiliarias to the user
-                    $user->imobiliarias()->attach($attach);
+                    $user->imobiliarias()->attach($attachments);
                 }
             );
 
