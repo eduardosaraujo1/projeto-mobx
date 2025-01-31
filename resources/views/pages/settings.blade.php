252a7c835b7a9c3cff1563b0987f00144574eebb
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-01-30 22:37:03 -0300

    fix: excessive padding on settings page

--- resources/views/pages/settings.blade.php
+++ resources/views/pages/settings.blade.php
@@ -1,24 +1,22 @@
 <x-app-layout>
-    <div class="px-4 py-12 mx-auto space-y-6 max-w-7xl">
-        <h2 class="mx-auto mb-5 text-2xl font-semibold leading-tight text-gray-800">
-            Configurações
-        </h2>
-        <div class="p-8 bg-white shadow sm:rounded-lg">
-            <div class="max-w-xl">
-                <livewire:settings.update-profile-information-form />
-            </div>
+    <h2 class="mx-auto mb-5 text-2xl font-semibold leading-tight text-gray-800">
+        Configurações
+    </h2>
+    <div class="p-8 bg-white shadow sm:rounded-lg">
+        <div class="max-w-xl">
+            <livewire:settings.update-profile-information-form />
         </div>
+    </div>
 
-        <div class="p-8 bg-white shadow sm:rounded-lg">
-            <div class="max-w-xl">
-                <livewire:settings.update-password-form />
-            </div>
+    <div class="p-8 bg-white shadow sm:rounded-lg">
+        <div class="max-w-xl">
+            <livewire:settings.update-password-form />
         </div>
+    </div>
 
-        <div class="p-8 bg-white shadow sm:rounded-lg">
-            <div class="max-w-xl">
-                <livewire:settings.delete-user-form />
-            </div>
+    <div class="p-8 bg-white shadow sm:rounded-lg">
+        <div class="max-w-xl">
+            <livewire:settings.delete-user-form />
         </div>
     </div>
 </x-app-layout>
