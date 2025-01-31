237078bc3d5aa16658a7630c141af27e06996fb3
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-01-31 05:15:54 -0300

    feat: create routes for adding new entries to database

--- resources/views/livewire/cadastro/imovel.blade.php
+++ resources/views/livewire/cadastro/imovel.blade.php
@@ -1,13 +1,14 @@
 <?php
 
 use Livewire\Volt\Component;
+use Livewire\Attributes\Layout;
 
-new class extends Component {
+new #[Layout('layouts.app')] class extends Component {
     //
 }; ?>
 
 <div>
-    //
+    Work in Progress
 </div>
 
 {{-- Must be live component because of error validation (no refreshing) --}}
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
     return redirect(Auth::check() ? '/imobiliaria' : '/login');
 });
 
+// navbar
 Route::get('imobiliaria', [ImobiliariaController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('imobiliaria.index');
@@ -30,16 +31,35 @@ Route::view('clientes', 'pages.client.index')
     ->middleware(['auth', 'verified'])
     ->name('client.index');
 
-Route::view('admin', 'pages.admin.index')
-    ->middleware(['auth', 'verified', 'admin'])
-    ->name('admin.index');
-
 Route::view('dashboard', 'pages.imobiliaria.dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+
+// user dropdown nav
+Route::view('admin', 'pages.admin.index')
+    ->middleware(['auth', 'verified', 'admin'])
+    ->name('admin.index');
+
 Route::view('configuracoes', 'pages.user.settings')
     ->middleware(['auth', 'verified'])
     ->name('settings');
+
+// cadastro
+Volt::route('imobiliaria/novo', 'cadastro.imobiliaria')
+    ->middleware(['auth', 'verified'])
+    ->name('imobiliaria.new');
+
+Volt::route('imovel/novo', 'cadastro.imovel')
+    ->middleware(['auth', 'verified'])
+    ->name('imovel.new');
+
+Volt::route('cliente/novo', 'cadastro.client')
+    ->middleware(['auth', 'verified'])
+    ->name('client.new');
+
+Volt::route('usuario/novo', 'cadastro.user')
+    ->middleware(['auth', 'verified'])
+    ->name('user.new');
 
 require __DIR__ . '/auth.php';
