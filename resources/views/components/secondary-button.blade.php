be3539f68c59e94fd1fc58695439adfb601dfa0d
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-02-06 23:17:02 -0300

    small: add href support for secondary button

--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,3 +1,17 @@
-<button {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150']) }}>
-    {{ $slot }}
-</button>
+@props([
+    "href",
+])
+@php
+    $class =
+        "inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-gray-700 uppercase transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25";
+@endphp
+
+@isset($href)
+    <a {{ $attributes->merge(["href" => $href, "class" => $class]) }}>
+        {{ $slot }}
+    </a>
+@else
+    <button {{ $attributes->merge(["type" => "button", "class" => $class]) }}>
+        {{ $slot }}
+    </button>
+@endisset
