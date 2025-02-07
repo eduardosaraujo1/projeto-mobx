59cd7ff08bfa31780b01f172a3efa2918d3ee71f
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-02-07 02:11:44 -0300

    feat: add instructions for deploying sail

---
+++ saildeploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ ! -f ./vendor/autoload.php ]; then
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v "$(pwd):/var/www/html" \
+    -w /var/www/html \
+    laravelsail/php84-composer:latest \
+    composer install --ignore-platform-reqs
+fi
+
+./vendor/bin/sail up -d
+./vendor/bin/sail npm install
+./vendor/bin/sail artisan key:generate
+./vendor/bin/sail artisan config:cache
+./vendor/bin/sail npm run dev
