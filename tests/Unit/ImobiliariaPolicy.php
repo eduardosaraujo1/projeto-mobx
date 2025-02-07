7d4bf9af134743a27dd729fc2f14fb8db394d95a
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-02-07 15:20:02 -0300

    feat: add colaborator to test (iteration 1)

--- tests/Unit/ImobiliariaPolicy.php
+++ tests/Unit/ImobiliariaPolicy.php
@@ -10,27 +10,41 @@ test('tests the update policy of imobiliaria', function () {
     $admin = User::factory()->make(['is_admin' => true]);
     $gerente = User::factory()->make();
     $imobiliaria = Imobiliaria::factory()->make();
+    $colaborador = User::factory()->make();
 
     // Manually create the pivot model instance
-    $role = new Role([
+    $managerRole = new Role([
         'user_id' => $gerente->id,
         'imobiliaria_id' => $imobiliaria->id,
         'role' => UserRole::GERENTE,
     ]);
+    $colabRole = new Role([
+        'user_id' => $colaborador->id,
+        'imobiliaria_id' => $imobiliaria->id,
+        'role' => UserRole::COLABORADOR,
+    ]);
 
     // Attach the pivot model to the relationship in memory
     $gerente->setRelation('imobiliarias', collect([$imobiliaria]));
-    $imobiliaria->setRelation('users', collect([$gerente]));
+    $colaborador->setRelation('imobiliarias', collect([$imobiliaria]));
+    $imobiliaria->setRelation('users', collect([$gerente, $colaborador]));
 
     // Manually associate the pivot attributes
-    $imobiliaria->users->first()->role = $role;
+    $colaborador->imobiliarias()->firstWhere('id', $imobiliaria->id)->role = $managerRole;
+    $gerente->imobiliarias()->firstWhere('id', $imobiliaria->id)->role = $managerRole;
+    $imobiliaria->users->firstWhere('id', $gerente->id)->role = $managerRole;
+    $imobiliaria->users->firstWhere('id', $colaborador->id)->role = $colabRole;
 
     // admin test
-    $result = (new ImobiliariaPolicy)->update($admin, $imobiliaria);
+    $policy = new ImobiliariaPolicy;
+    $result = $policy->update($admin, $imobiliaria);
     expect($result)->toBeTrue('Admin cannot update imobiliaria');
 
     // user manager test
-    $result = (new ImobiliariaPolicy)->update($gerente, $imobiliaria);
-    expect($result)->toBeTrue('Manager cannot update imobiliaria');
+    $result = $policy->update($gerente, $imobiliaria);
+    expect($result)->toBeTrue('Manager should update imobiliaria');
 
+    // user colaborator test
+    $result = $policy->update($colaborador, $imobiliaria);
+    expect($result)->toBeFalse('Colaborador must not update imobiliaria');
 });
